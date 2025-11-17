15f5e9682a75f9e1ab61c02eefe8509a22e00772
josejesus823 <jose1030vargas@gmail.com>
2025-11-17 02:09:58 -0500

    feat: update index.html structure and enhance article creation functionality in scriptRefactored.js

--- checkpoint/scriptRefactored.js
+++ checkpoint/scriptRefactored.js
@@ -1,5 +1,6 @@
 const createForm = document.getElementById("add_form");
 createForm.addEventListener('click', function () {
+
     const imageUrl = document.getElementById("image_url").value.trim();
     const getInputValueTitle = document.getElementById('title').value.trim();
     const getInputValueDescription = document.getElementById('description').value.trim();
@@ -11,31 +12,32 @@ createForm.addEventListener('click', function () {
         const getElementTemplate = document.getElementById('article-template');
         const cloneElementTemplate = getElementTemplate.content.cloneNode(true);
 
+
+        const dateObj = new Date(getInputValueDate);
+        const dateFormatted = dateObj.toLocaleDateString("en-US", {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+
         cloneElementTemplate.querySelector('.article-image').src = imageUrl;
         cloneElementTemplate.querySelector('.article-image').alt = getInputValueTitle;
         cloneElementTemplate.querySelector('.article-title').textContent = getInputValueTitle;
         cloneElementTemplate.querySelector('.article-description').textContent = getInputValueDescription;
-        cloneElementTemplate.querySelector('.article-date').textContent = getInputValueDate;
+        cloneElementTemplate.querySelector('.article-date').textContent = dateFormatted;
         cloneElementTemplate.querySelector('.article-area').textContent = getInputValueArea;
 
         document.getElementById('articles-container').appendChild(cloneElementTemplate);
+
+        document.getElementById("image_url").value = "";
+        document.getElementById("title").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("area").value = "";
+
     }
     else {
         alert("Pleas fill all inputs");
     }
 
-})
-
-document.getElementById('date').addEventListener('change', function(){
-    const value = this.value;
-    if (!value) return;
-
-    const date = new Date(value);
-    const formatted = date.toLocaleDateString('en-US', {
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    });
-
-    document.getElementById("formatted-date").textContent = formatted;
 })
